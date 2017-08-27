13290de00547a943643e3f2a6b1176475c634679
Tianhang Liu <tianhanl@usc.edu>
2017-08-26 23:22:05 -0700

    add loading component

---
+++ src/components/loading.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="c-loading">
+    <div :class="animationName"></div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'loading',
+  props: {
+    animationName: String,
+    width: Number,
+    height: Number
+  },
+  data() {
+    return {
+    }
+  }
+}
+
+</script>
+<style>
+
+</style>
+

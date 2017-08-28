c09a61b1934154e55143bb1d648c15f897ab69cc
Tianhang Liu <tianhanl@usc.edu>
2017-08-27 22:01:31 -0700

    add basic setup for square animation

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="c-loading">
-    <div :class="animationName"></div>
+    <div :class="animation"></div>
   </div>
 </template>
 <script>
@@ -8,7 +8,7 @@
 export default {
   name: 'loading',
   props: {
-    animationName: String,
+    animation: String,
     width: Number,
     height: Number
   },
@@ -20,6 +20,31 @@ export default {
 
 </script>
 <style>
+.square {
+  position: relative;
+  box-sizing: border-box;
+  width: 120px;
+  height: 120px;
+}
 
+.suqare::before {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  border-top: 2px solid #F07883;
+  border-right: 2px solid #F07883;
+}
+
+.square::after {
+  content: '';
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  border-left: 2px solid #F07883;
+  border-bottom: 2px solid #F07883;
+}
 </style>
 
